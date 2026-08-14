d3aa357eaead3dc59b1f2dd6a8f0621a89163c6d
abassunarya16 <abassunarya16@gmail.com>
2026-08-14 12:01:10 +0700

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // Pusat Konfigurasi Versi Aplikasi Cozycs Farm (Single Source of Truth)
-var APP_VERSION = '1.1.4';
+var APP_VERSION = '1.1.5';
 
 var Helper = (function() {
 
